f4dfe07286848d4afe8a3fbc0bb8dbd542844ab3
Stewart Gliniecki <stewgliniecki@gmail.com>
2021-04-21 20:31:34 +0100

    Added gameboard array

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 var gameBoard = (function() {
     'use strict';
 
-    const personFactory = (name, counter) => {
+    const personFactory = (name, counter, turn) => {
         const sayHello = () => console.log('hello!');
-        return { name, counter, sayHello };
+        return { name, counter, turn, sayHello };
       };
-      const playerOne = personFactory('Player One', 'x')
-      const playerTwo = personFactory('Player Two', 'o')
+      const playerOne = personFactory('Player One', 'x' , true)
+      const playerTwo = personFactory('Player Two', 'o' , false)
 
+      console.log(playerOne.counter)
+      console.log(playerTwo.counter)
+      console.log(playerTwo.turn)
+
+    const gameBoardArray = [];
+
+    const winningSequences = 
+    [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    
 
 
   })();
