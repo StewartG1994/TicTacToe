ec147086ce11ce8c11f400503df6d48d69270569
Stewart Gliniecki <stewgliniecki@gmail.com>
2021-09-21 13:00:51 +0100

    Finished for now

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-
+const player = (name)=>{
+    return {name}
+}
 
 let gameBoard = (function(){
-
+ 
     let gridItem = document.querySelectorAll('.grid');
-    let winElement = document.querySelector('.win')
+    let winElement = document.querySelector('.win');
+    let restartElem  =document.querySelector('.restart')
 
-    let counter = 0
+    let counter = 0;
     let playerOneTurn = true;
     let playerTwoTurn = false;
+    let playerOne = null;
+    let playerTwo = null;
+    let playerOneWin = null;
+    let playerTwoWin = null;
 
     let playerOneArray = [];
     let playerTwoArray = [];
@@ -24,47 +31,68 @@ let gameBoard = (function(){
         [2,4,6]
     ]
 
+    const playGame = () =>  {
+        let playerOneId = document.querySelector('.p1Name');
+        let playerTwoId = document.querySelector('.p2Name');
+        let start = document.querySelector('.start');
+        
+        start.addEventListener('click', () =>{
+           playerOne = player(playerOneId.value);
+           playerTwo = player(playerTwoId.value);
+           winElement.textContent = (playerOne.name + " make your move")
+           turn()
+        })
+    }
+    
+    const pReload = () =>{
+
+        restartElem.addEventListener('click', ()=> {
+            location.reload();
+            return false;
+
+          })      
+    }  
 
     const winnerCheck = ()=> 
         winningSequences.forEach(item => {
-            if (item.every(elem => playerOneArray.includes(elem))){winElement.textContent = 'Player One Wins'}
-            else if (item.every(elem => playerTwoArray.includes(elem))) {winElement.textContent = 'Player Two Wins'}
-            
-
+       
+            if (item.every(elem => playerOneArray.includes(elem))){winElement.textContent = playerOne.name + ' wins'; playerOneWin = true; counter++}
+            else if (item.every(elem => playerTwoArray.includes(elem))) {winElement.textContent = playerTwo.name + ' wins'; playerTwoWin = true; counter++}
+            else if (counter === 9 && playerOneWin === null && playerTwoWin === null) {winElement.textContent = 'Draw'}
         })
 
     const turn = () =>{
-        console.log(winningSequences)
+
         gridItem.forEach(item =>{item.addEventListener('click', () =>{
-      
-            if (playerOneTurn === true && item.textContent === ''){
+    
+             if (playerOneTurn === true && item.textContent === '' && playerOneWin === null & playerTwoWin === null){
                 item.textContent = 'X';
                 let playerOneData = item.getAttribute('data-number')
                 playerOneArray.push(parseInt(playerOneData));
-                winnerCheck()
-                console.log(playerOneArray)
+                winElement.textContent = playerTwo.name + ' make your move';
+                winnerCheck()        
                 playerOneTurn = false;
-                playerTwoTurn = true;
+                playerTwoTurn = true;   
                 
             }
 
-            else if (playerTwoTurn === true && item.textContent === ''){
+            else if (playerTwoTurn === true && item.textContent === '' && playerOneWin === null & playerTwoWin === null){
                 item.textContent = 'O';
                 let PlayerTwoData = item.getAttribute('data-number')
                 playerTwoArray.push(parseInt(PlayerTwoData))
+                winElement.textContent = playerOne.name + ' make your move'
                 winnerCheck()
-                console.log(playerTwoArray)
                 playerOneTurn = true;
                 playerTwoTurn = false;
-
+                
         }
+
+           if  (playerOneWin || playerTwoWin === true ) {restartElem.style.color = 'red'; restartElem.style.border = '3px solid red'}
         })})
+       
     }
-
-
-    return {turn}
+    return { pReload, playGame}
 })()
 
-
-gameBoard.turn()
-
+gameBoard.playGame()
+gameBoard.pReload()
